3a455ac34ddbe8577c64d978c8f1488d867407f7
eduardonk9999 <thiago.novak7@gmail.com>
2023-06-14 13:53:04 -0300

    feat: tipos de variaveis

--- variaveis.c
+++ variaveis.c
@@ -1,8 +1,27 @@
 #include <stdbool.h>
 
 int main() {
-    int idade;
+    int idade = 31;
     float salario = 1250.75;
     double porcentagem_desconto = 2.5;
     bool estaAprovado = false;
+    char genero = "M";
+
+    printf("Ola, mundo!");
+    printf("Idade: %i", idade);
+    return 0;
+
 }
+/*
+Para suprir a limitacao dos calores que uma variavel pode
+assumir pelo seu tipo, foram criados modificadores de tipo:
+
+
+ Usinend = Usado para especificar que a variavel armazenara
+ somente a parte positiva do numero.
+
+ Short = Que reduz o espaco reservado memoria.
+
+ Long = Que aumenta o espaco reservado na memoria.
+
+*/
