8930a5a3844628d3187ef37cf28060d3ae59adb3
eduardonk9999 <thiago.novak7@gmail.com>
2023-06-21 13:49:47 -0300

    feat: logica desconto salario bruto

--- pay.c
+++ pay.c
@@ -18,13 +18,28 @@ void main(){
   };
 
   int desconto_inss = 0.0;
+  int salario_bruto = 2300;
 
   // Criar um for para calcular o desconto do INSS, salarioBruto * aliquota;
   // essa aliquota vai ser dentro do for, vai ser a aliquota do indice do for
+  // Validacao dentro do for, salario_bruto <= limite do indice do for
+  for(int aliquota = 0; aliquota < 4; aliquota++) {
+    if(salario_bruto <= tabela_inss[aliquota]) {
+      desconto_inss = salario_bruto * aliquota;
+      break;
+    }
+  }
+  printf("Desconto INSS: %d\n", desconto_inss);
+
 
 
   // Criar um for para calcular o desconto do IRRF, salarioBruto * aliquota;
   //base_calculo_ir = salario_bruto - desconto_inss
   // desconto_ir = 0.0
+  // Validacao dentro do for, base_calculo_ir <= limite: do indice do for
 
+
+  // Cálcular o salário líquido
+  // salario_liquido = salario_bruto - desconto_inss - desconto_ir
+  
 }
